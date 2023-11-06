9180a9db6bb09b46fc5ddbd3ac6d251d6837edc1
HansVercruysse <hans.vercruysse@ugent.be>
2023-11-06 16:58:43 +0100

    setup

---
+++ project_22/src/main/java/controllers/UserController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
---
+++ project_22/src/main/java/domain/user/User.java
@@ -0,0 +1,7 @@
+package domain.user;
+
+public class User {
+    private Long id;
+    private String firstname;
+    private String lastname;
+}
